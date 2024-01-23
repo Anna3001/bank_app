03ae549132d15db3f7b0a25c4dd84988c12778b9
Anna3001 <aniaw3001@icloud.com>
2024-01-23 23:57:02 +0100

    second commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+user1 = User.create!(
+  email: 'user1@pl.pl',
+  password: 'pass3#WO11rd!?',
+)
+
+# User 2
+user2 = User.create!(
+  email: 'user2@pl.pl',
+  password: 'G$7tL9zW1qXp2',
+)
+
+# User 3
+user3 = User.create!(
+  email: 'user3@pl.pl',
+  password: 'uX5vG#zLmY9sR3kPw',
+)
+
+# User 4
+user4 = User.create!(
+  email: 'user4@pl.pl',
+  password: 'u^5FzG8sW!rLkZ2vq;',
+)
+
